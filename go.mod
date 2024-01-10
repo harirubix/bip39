5d48478b21421fda2d0a608975ff180b05c0fec2
harirubix <hari@rubix.net>
2024-01-10 17:57:07 +0530

    sign using bip39 keys

--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module dwebchap/wal
 go 1.20
 
 require (
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/decred/dcrd/dcrec/secp256k1 v1.0.4
 	github.com/tyler-smith/go-bip32 v1.0.0
 	github.com/tyler-smith/go-bip39 v1.1.0
 )
 
+require github.com/decred/dcrd/dcrec/secp256k1/v2 v2.0.0 // indirect
+
 require (
 	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
 	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
+	github.com/mr-tron/base58 v1.2.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
 )
--- test.go
+++ test.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
+	"encoding/hex"
+
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/decred/dcrd/dcrec/secp256k1"
 	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
 )
@@ -17,9 +23,6 @@ func main() {
 
 	// Create master private key from seed
 	computerVoiceMasterKey, _ := bip32.NewMasterKey(seed)
-
-	fmt.Println(" mastekey ", computerVoiceMasterKey)
-	fmt.Println("masterpublic ", computerVoiceMasterKey.PublicKey())
 
 	// Map departments to keys
 	// There is a very small chance a given child index is invalid
@@ -37,8 +40,36 @@ func main() {
 	departmentAuditKeys["Engineering"] = departmentKeys["Engineering"].PublicKey()
 	departmentAuditKeys["Customer Support"] = departmentKeys["Customer Support"].PublicKey()
 
-	// Print public keys
-	for department, pubKey := range departmentAuditKeys {
-		fmt.Println(department, departmentKeys[department], pubKey)
+	// dept, result := departmentKeys["Sales"].Serialize()
+
+	num := departmentKeys["Sales"].B58Serialize()
+	value := strings.TrimSpace(num[3:])
+	back := base58.Decode(value)
+	encodedString := hex.EncodeToString(back)
+	pkBytes, err := hex.DecodeString(encodedString)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+	privKey, pubKey := secp256k1.PrivKeyFromBytes(pkBytes)
+
+	message := "test message"
+	messageHash := chainhash.HashB([]byte(message))
+	signature, err := privKey.Sign(messageHash)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Serialize and display the signature.
+	fmt.Printf("Serialized Signature: %x\n", signature.Serialize())
+
+	// Verify the signature for the message using the public key.
+	verified := signature.Verify(messageHash, pubKey)
+	fmt.Printf("Signature Verified? %v\n", verified)
+
+	// Print public keys
+	// for department, pubKey := range departmentAuditKeys {
+	// 	fmt.Println(department, departmentKeys[department], pubKey)
+	// }
 }
